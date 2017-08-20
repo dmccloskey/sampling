84e08618ff93abec43d5e5a067931a5801ef4c8d
Douglas McCloskey <dmccloskey87@gmail.com>
2017-08-20 12:03:53 +0200

    [FOLDER] changed folder name

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -8,8 +8,8 @@ from cobra.manipulation.modify import convert_to_irreversible
 import csv,json,sys
 
 # Dependencies
-from cobra_utilities.optGpSampler_sampling import optGpSampler_sampling
-from cobra_utilities.matlab_sampling import matlab_sampling
+from cobra_sampling.optGpSampler_sampling import optGpSampler_sampling
+from cobra_sampling.matlab_sampling import matlab_sampling
 
 from . import data_dir
 
